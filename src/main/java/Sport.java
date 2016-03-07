d8164d56b4738f54dbcb2f435140bcfa96d3d7ff
Kyle Derrick and Michael Schennum <mikeschenphoto@gmail.com>
2016-03-07 10:23:53 -0800

    update databaseRule

--- src/main/java/Sport.java
+++ src/main/java/Sport.java
@@ -2,3 +2,30 @@ import org.sql2o.*;
 import java.util.List;
 import java.util.ArrayList;
 import org.apache.commons.lang.WordUtils;
+
+
+public class Sport {
+  private String name;
+  private int user_id;
+  private int id;
+
+  public Sport (String name, int user_id) {
+    this.name = name;
+    this.user_id = user_id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getUserId() {
+    return user_id;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+
+
+}
